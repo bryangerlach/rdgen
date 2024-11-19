aa5e22c8209b83dc12c43facaf726daa95bfda74
Bryan Gerlach <bryan.r.gerlach@gmail.com>
2024-11-19 10:38:16 -0600

    fix for pre 1.3.3 versions

--- rdgenerator/views.py
+++ rdgenerator/views.py
@@ -166,6 +166,16 @@ def generator_view(request):
                 url = 'https://api.github.com/repos/'+_settings.GHUSER+'/rdgen/actions/workflows/generator-android.yml/dispatches'
             else:
                 url = 'https://api.github.com/repos/'+_settings.GHUSER+'/rdgen/actions/workflows/generator-windows.yml/dispatches'
+            ####breaking changes were made in 1.3.3 version, so if 1.3.2 or lower, use:
+            if version == '1.3.2' or version == '1.3.1' or version == '1.3.0':
+                if platform == 'windows':
+                    url = 'https://api.github.com/repos/'+_settings.GHUSER+'/rdgen/actions/workflows/pre133-generator-windows.yml/dispatches' 
+                elif platform == 'linux':
+                    url = 'https://api.github.com/repos/'+_settings.GHUSER+'/rdgen/actions/workflows/pre133-generator-linux.yml/dispatches'  
+                elif platform == 'android':
+                    url = 'https://api.github.com/repos/'+_settings.GHUSER+'/rdgen/actions/workflows/pre133-generator-android.yml/dispatches'
+                else:
+                    url = 'https://api.github.com/repos/'+_settings.GHUSER+'/rdgen/actions/workflows/pre133-generator-windows.yml/dispatches'
 
             #url = 'https://api.github.com/repos/'+_settings.GHUSER+'/rustdesk/actions/workflows/test.yml/dispatches'  
             data = {
