87f587699c1ab136254c63dc2bb74a3c58299ad4
Bryan Gerlach <bryan.r.gerlach@gmail.com>
2025-02-05 00:32:59 -0600

    add save/load configuration

--- rdgenerator/forms.py
+++ rdgenerator/forms.py
@@ -33,6 +33,8 @@ class GenerateForm(forms.Form):
     #Visual
     iconfile = forms.FileField(label="Custom App Icon (in .png format)", required=False, widget=forms.FileInput(attrs={'accept': 'image/png'}))
     logofile = forms.FileField(label="Custom App Logo (in .png format)", required=False, widget=forms.FileInput(attrs={'accept': 'image/png'}))
+    iconbase64 = forms.CharField(required=False)
+    logobase64 = forms.CharField(required=False)
     theme = forms.ChoiceField(choices=[
         ('light', 'Light'),
         ('dark', 'Dark'),
