6167b8f61d605e9e1f64e64eb3c06e04827b20e7
Bryan Gerlach <bryan.r.gerlach@gmail.com>
2025-02-27 11:37:46 -0600

    update bridge

--- rdgenerator/forms.py
+++ rdgenerator/forms.py
@@ -5,7 +5,7 @@ class GenerateForm(forms.Form):
     #Platform
     platform = forms.ChoiceField(choices=[('windows','Windows'),('linux','Linux (currently unavailable)'),('android','Android'),('macos','macOS')], initial='windows')
     version = forms.ChoiceField(choices=[('master','nightly'),('1.3.8','1.3.8'),('1.3.7','1.3.7'),('1.3.6','1.3.6'),('1.3.5','1.3.5'),('1.3.4','1.3.4'),('1.3.3','1.3.3')], initial='1.3.8')
-    help_text="'master' is the development version (nightly build) with the latest features but may be less stable")
+    help_text="'master' is the development version (nightly build) with the latest features but may be less stable"
     delayFix = forms.BooleanField(initial=True, required=False)
 
     #General
