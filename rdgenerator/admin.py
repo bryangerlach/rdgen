dcc54b34667a8375f05c1c1cfcc1ebe0c22469fb
素言 <59853948+cjl-cjl@users.noreply.github.com>
2026-07-27 09:34:09 +0800

    Delete rdgenerator directory

--- rdgenerator/admin.py
+++
@@ -1,3 +0,0 @@
-from django.contrib import admin
-
-# 在这里注册你的模型
--- rdgenerator/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class RdgeneratorConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'rdgenerator'
